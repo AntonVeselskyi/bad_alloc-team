da269ad3bf5bf8df8fafc3fbd4fbbe916294653e
Borys Karpliak <borys.karpliak@gmail.com>
2019-02-13 21:07:57 +0200

    D2019: Add data scan

--- demo_hc2019/solution.py
+++ demo_hc2019/solution.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python
 import sys
+from enum import Enum
 
+
+# 0 - tomato
+# 1 - mushroom
+
+class Status(Enum):
+    FREE = 1
+    TAKEN = 2
+    BSTD = 3
+
+
+class PzCell(int):
+    def __init__(self, num, status=Status.FREE):
+        super(PzCell, self).__new__(PzCell, num)
+        self.status = status
 
 
 def ABreadline(f):
     return [int(x) for x in next(f).split()]
 
 
+cells = []
 with open(sys.argv[1]) as f:
-    vid_num, ep_num, req_num, cache_num,cache_size  = ABreadline(f)
+    rows, columns, min_per_slice, max_cells = ABreadline(f)
+    for line in f.readlines():
+        cells.append([PzCell(0) if c == 'T' else PzCell(1) for c in line[:-1]])
+
+
