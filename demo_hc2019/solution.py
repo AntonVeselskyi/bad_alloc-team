c1a94cd8e3231adb9e71263ea9c2ab64b51210e3
Borys Karpliak <borys.karpliak@gmail.com>
2019-02-13 22:06:36 +0200

    D2019: Added Manager fetus.

--- demo_hc2019/solution.py
+++ demo_hc2019/solution.py
@@ -28,4 +28,31 @@ with open(sys.argv[1]) as f:
     for line in f.readlines():
         cells.append([PzCell(0) if c == 'T' else PzCell(1) for c in line[:-1]])
 
+    i = 0
+    j = 1
+    slices = []
 
+    while i < rows:
+        slice = find_slice(cells, min_per_slice, max_cells, (i, j),
+                           next((i for i, v in enumerate(cells[i][j:]) if v.status == Status.TAKEN), default=columns))
+
+        if not slice:
+            if j < columns - 1:
+                j += 1
+            else:
+                j = 0
+                i += 1
+            continue
+        slices.append(slice)
+
+        for i in range(slice[1][0], slice[1][1] + 1):
+            for j in range(slice[0][0], slice[0][1] + 1):
+                cells[i][j].status = Status.TAKEN
+
+        if j < columns - slice[0][1]:
+            j += slice[0][1]
+        else:
+            j = 0
+            i += 1
+
+print(slices)
