b31fc02203b2f12df45706a19888959d639e8073
AntonVeselskyi <anton.veselskyi@gmail.com>
2019-02-28 22:41:25 +0200

    2019: COLLLLLLLLLLLLLLLX

--- hc2019/solution.py
+++ hc2019/solution.py
@@ -14,6 +14,9 @@ class Photo:
     def __eq__(self, other):
         return self.id == other.id
 
+    def __lt__(self, other):
+        return len(self.tags) < len(other.tags)
+
 
 def score(photo1: Photo, photo2: Photo):
     return min(len(photo1.tags - photo2.tags), len(photo2.tags - photo1.tags), len(photo1.tags & photo2.tags))
@@ -22,7 +25,7 @@ def score(photo1: Photo, photo2: Photo):
 def ABreadline(f):
     return [int(x) for x in next(f).split()]
 
-sys.argv.append('a_example.txt')
+#sys.argv.append('a_example.txt')
 with open(sys.argv[1]) as f:
     N = ABreadline(f)[0]
     photos = []
@@ -39,27 +42,23 @@ with open(sys.argv[1]) as f:
         photos.append(Photo(photo_num, orient, tags))
 
     # Manager
-    sys.stdout.flush()
-    print("W hi MMMMMMMMMMMMMMMMMMMMMMMMMMM")
-    print(datetime.datetime.now())
-    sys.stdout.flush()
-    gg = sorted(photos)
-    print(datetime.datetime.now())
-    sys.stdout.flush()
+
+    photos = sorted(photos)
+
+    chunck = int(N/30) + 2;
     res = [photos[0]]
     del photos[0]
-    l = len(photos)
-    for i in photos[:]:
+    print(N)
+    for n in range(len(photos[:])-1):
+        #print("progress", n)
         max_score_for_i = 0
-        print(f"{i.id} in {l} proceeded")
-        before = datetime.datetime.now()
-        best_second_photo = max(photos[:], key=lambda p: score(i, p))
+        best_second_photo = max(photos[:chunck if chunck < len(photos) else len(photos)-1 ], key=lambda p: score(res[-1], p))
 
         res.append(best_second_photo)
         photos.remove(best_second_photo)
 
+    res.append(photos[-1])
     fr = open("./res.txt", "w+")
     fr.write(str(len(res)) + "\n")
-
-    fr.write("\n".join((str(i) for i in res)))
+    fr.write("".join((str(i.id)+'\n' for i in res)))
     fr.close()
