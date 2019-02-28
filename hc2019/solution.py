a6f19196feb8d1ddd5e5f440881b4adc5e531fe2
AntonVeselskyi <anton.veselskyi@gmail.com>
2019-02-28 20:49:31 +0200

    2019: Added manager, output and simple logic.

--- hc2019/solution.py
+++ hc2019/solution.py
@@ -10,6 +10,9 @@ class Photo:
         self.orient = orie
         self.tags = tags
 
+    def __eq__(self, other):
+        return self.id == other.id
+
 
 def score(photo1: Photo, photo2: Photo):
     return min(len(photo1.tags - photo2.tags), len(photo2.tags - photo1.tags), len(photo1.tags & photo2.tags))
@@ -34,5 +37,23 @@ with open(sys.argv[1]) as f:
             tags.add(word)
         photos.append(Photo(photo_num, orient, tags))
 
-    for ph in photos:
-        print(ph.orient, ph.tags)
+    # Manager
+    res = [photos[0]]
+    del photos[0]
+    for i in photos:
+        max_score_for_i = 0;
+        best_second_photo = 0;
+        for j in photos:
+            jscore = score(i, j)
+            if jscore > max_score_for_i:
+                max_score_for_i = jscore
+                best_second_photo = j
+
+        res.append(best_second_photo)
+        photos.remove(best_second_photo)
+
+    fr = open("./res.txt", "w+")
+    fr.write(str(len(res)) + "\n")
+    for ph in res:
+        fr.write(ph.id + "\n")
+    fr.close()
