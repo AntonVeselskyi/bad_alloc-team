921ea115e1bc1f4ed01ca35eaf1fecc2577892ff
AntonVeselskyi <anton.veselskyi@gmail.com>
2019-02-28 20:12:42 +0200

    2019: Read input.

--- hc2019/solution.py
+++ hc2019/solution.py
@@ -3,16 +3,32 @@ import sys
 from enum import Enum
 
 
+class Photo:
+
+    def __init__(self, id, orie, tags):
+        self.id = id
+        self.orient = orie
+        self.tags = tags
+
 
 def ABreadline(f):
     return [int(x) for x in next(f).split()]
 
-def ABread_by_char(bs):
-        bs= []
-        for _ in range(N):
-            bs.append( int(f.read(1)) )
-        f.read(1) # skip '\n'
-        return bs;
 
 with open(sys.argv[1]) as f:
-    rows, columns, min_per_slice, max_cells = ABreadline(f)
+    N = ABreadline(f)[0]
+    photos = []
+    for photo_num in range(N):
+        orient = '';
+        tags = set()
+        for n, word in enumerate(next(f).split()):
+            if n == 0:
+                orient = str(word)
+                continue
+            if n == 1:
+                continue
+            tags.add(word)
+        photos.append(Photo(photo_num, orient, tags))
+
+    for ph in photos:
+        print(ph.orient, ph.tags)
