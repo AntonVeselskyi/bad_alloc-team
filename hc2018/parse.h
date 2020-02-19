e46f066a7bef8bb043f097cc7772e821d17c4d90
Oleksandr <anton.vesselskyi@gmail.com>
2020-02-19 20:44:43 +0200

    Parser was added

---
+++ hc2018/parse.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+queue<Ride*> parse_data(const char *fileName, Settings &params)
+{
+    queue<Ride*> result;
+
+    int r = 0, c = 0, f = 0, n = 0, b = 0, t = 0;
+    fstream file;
+    file.open(fileName);
+
+    if (file.is_open())
+    {
+        file >> params.R >> params.C >> params.F >> params.N >> params.B >> params.T;
+    }
+
+
+    int index = 0;
+    while (index < n)
+    {
+        Ride *rd = new Ride();
+        rd->index = index;
+
+        int a = 0, b = 0;
+        file >> a >> b;
+        rd->start_location = make_pair(a, b);
+
+        int x = 0, y = 0;
+        file >> x >> y;
+        rd->end_location = make_pair(x, y);
+
+        file >> rd->start >> rd->end;
+
+        result.push(rd);
+        ++index;
+    }
+
+    file.close();
+    cout << r << c << f << n << b << t << endl;
+
+    return result;
+}
---
+++ hc2018/ride.h
@@ -0,0 +1,8 @@
+struct Ride
+{
+    int index;
+    pair<int, int> start_location;
+    pair<int, int> end_location;
+    int start;
+    int end;
+};
