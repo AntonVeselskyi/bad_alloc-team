1703ef70cd6d05c45407f401cde3e5120646ba9c
Borys Karpliak <borys.karpliak@gmail.com>
2020-02-20 21:09:14 +0200

    add lib sort function

---
+++ hc2020/libsort.h
@@ -0,0 +1,24 @@
+//
+// Created by Borys on 20.02.2020.
+//
+
+
+#include "entities.h"
+
+#include <numeric>
+#include <list>
+#include <algorithms>
+
+long long lib_score(list<Lib>& libs)
+{
+    for (Lib &lib: libs)
+    {
+        for(Book *book:lib.book_set)
+        {
+            lib.j_score += book->award;
+        }
+        lib.j_score /= lib.books_per_day;
+    }
+
+    std::sort(libs.begin(), libs.end());
+}
