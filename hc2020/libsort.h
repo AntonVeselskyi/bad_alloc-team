0c6f6cf3c1ccb6a2d3e1da4757f14013907fa369
Borys Karpliak <borys.karpliak@gmail.com>
2020-02-20 22:14:07 +0200

    count final library score

--- hc2020/libsort.h
+++ hc2020/libsort.h
@@ -12,15 +12,20 @@ long long lib_score(list<Lib>& libs, int days_left)
 {
     for (Lib &lib: libs)
     {
+        long long books_to_scan = (days_left - lib.sign_length) * lib.books_per_day;
         for(Book *book:lib.book_set)
         {
+            if (books_to_scan <= 0)
+            {
+                break;
+            }
+
             if (book->is_not_scanned)
             {
                 lib.j_score += book->award;
+                books_to_scan--;
             }
         }
-        lib.j_score /= lib.books_per_day;
     }
-
     libs.sort();
 }
