7661094307a65d94b95c88f5536c90939f1356bf
Yurii Karabas <1998uriyyo@gmail.com>
2020-03-12 18:00:53 +0200

    Add Basic entities

---
+++ reply2020/entities.h
@@ -0,0 +1,46 @@
+//
+// Created by Yurii Karabas on 3/12/20.
+//
+
+#ifndef REPLY2020_ENTITIES_H
+#define REPLY2020_ENTITIES_H
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <utility>
+
+using Pos = std::pair<int, int>
+
+using namespace std;
+
+enum Place {
+    UNAVAILABLE = '#',
+    DEV_FREE = '_',
+    PM_FREE = 'M',
+    DEV_USED = '*',
+    PM_USED = '$',
+};
+
+enum Role {
+    DEV,
+    PM
+};
+
+struct Room {
+    int width;
+    int heigth;
+    vector<vector<Place>> area;
+};
+
+struct User {
+    std::string company;
+    uint8_t bonus;
+    std::vector<std::string> skills;
+    Role role;
+    Pos pos;
+};
+
+struct
+
+#endif //REPLY2020_ENTITIES_H
