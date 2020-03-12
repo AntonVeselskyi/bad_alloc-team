be39e3ae88c329b54ea339057871aea3bca0589a
Yurii Karabas <1998uriyyo@gmail.com>
2020-03-12 20:18:27 +0200

    Fix errors at entities.h

--- reply2020/entities.h
+++ reply2020/entities.h
@@ -35,27 +35,6 @@ struct Room {
     vector<vector<Place>> area;
 };
 
-long long score(const User &a, const User &b)
-{
-
-    //work potential
-    long WP = 0,
-    //bonus potential
-         BP = 0;
-    std::list<int> skills_intersection;
-    std::set_intersection(a.skills.begin(), a.skills.end(), b.skills.begin(), b.skills.end(),
-                          std::back_inserter(skills_intersection));
-
-    std::list<int> skills_union;
-    std::set_union(a.skills.begin(), a.skills.end(), b.skills.begin(), b.skills.end(),
-                   std::back_inserter(skills_union));
-
-    WP = skills_intersection.size() + (skills_union.size() - skills_intersection.size())
-    BP = a.company == b.company ? a.bonus * b.bonus : 0;
-
-    return WP+BP;
-}
-
 struct User {
     std::string company;
     int bonus;
@@ -63,5 +42,27 @@ struct User {
     Role role;
     Pos pos = NOT_SET;
 };
+
+long long score(const User &a, const User &b)
+{
+
+    //work potential
+    long WP = 0,
+    //bonus potential
+         BP = 0;
+    std::list<string> skills_intersection;
+    std::set_intersection(a.skills.begin(), a.skills.end(), b.skills.begin(), b.skills.end(),
+                          std::back_inserter(skills_intersection));
+
+    std::list<string> skills_union;
+    std::set_union(a.skills.begin(), a.skills.end(), b.skills.begin(), b.skills.end(),
+                   std::back_inserter(skills_union));
+
+    WP = skills_intersection.size() + (skills_union.size() - skills_intersection.size());
+    BP = a.company == b.company ? a.bonus * b.bonus : 0;
+
+    return WP+BP;
+}
+
 
 #endif //REPLY2020_ENTITIES_H
