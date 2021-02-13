8cc0aa13c109edcb82462811532f4e7ef5b83ba4
Yurii Karabas <1998uriyyo@gmail.com>
2021-02-13 18:54:25 +0200

    Update decider

--- demo_hc2021/decider.h
+++ demo_hc2021/decider.h
@@ -5,11 +5,34 @@
 #ifndef HC2018_DECIDER_H
 #define HC2018_DECIDER_H
 
+#include <tuple>
+
 #include "entities.h"
+#include "brute_force_pizzas.h"
 
-void decide(Pizza) {
 
+int score(vector<pizzas_iter>& iters) {
 
+}
+
+optional<Delivery> decide(Pizzas &pizzas, int& two_teams, int& three_teams, int& four_teams) {
+    vector<vector<pizzas_iter>> options;
+
+    if (two_teams > 0) {
+        auto for_two = best_pizzas_for_2(pizzas);
+        if (for_two.size() > 0)
+            options.push_back(move(for_two));
+    }
+    if (three_teams > 0) {
+        auto for_three = best_pizzas_for_2(pizzas);
+        if (for_three.size() > 0)
+            options.push_back(move(for_three));
+    }
+    if (four_teams > 0) {
+        auto for_four = best_pizzas_for_2(pizzas);
+        if (for_four.size() > 0)
+            options.push_back(move(for_four));
+    }
 }
 
 
