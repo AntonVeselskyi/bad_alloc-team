8b5b41b37d049eb3c0d132c3aa9b9d0cd6e4d16e
AntonVeselskyi <777TAhove>
2021-02-13 19:14:24 +0200

    best_pizzas_for_2 basic implementation.

--- demo_hc2021/brute_force_pizzas.h
+++ demo_hc2021/brute_force_pizzas.h
@@ -9,7 +9,30 @@
 #include <functional>
 #include "entities.h"
 
-auto best_pizzas_for_2(Pizzas &map) {
+auto best_pizzas_for_2(Pizzas &pizzas)
+{
+    if(pizzas.empty())
+        return;
 
-    return std::make_tuple(map.begin(), map.begin());
+    size_t best_score = 0;
+    pizzas_iter a_iter = begin(pizzas), b_iter = begin(pizzas);
+
+    for(auto &a : pizzas)
+        for(auto &b : pizzas)
+        {
+            auto a_set = a.first, b_set = b.first;
+            set<std::string> intersection;
+            std::set_intersection(begin(a_set), end(a_set), begin(b_set), end(b_set), intersection);
+
+            size_t cur_res = intersection.size() * intersection.size();
+            if(best_score < cur_res)
+            {
+                best_score = cur_res;
+                a_iter = a;
+                b_iter = b;
+
+            }
+        }
+
+            return std::make_tuple(a_iter, b_iter);
 }
