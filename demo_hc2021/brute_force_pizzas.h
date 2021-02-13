2fa713ef460b227f73b801ba7e809dcc63ebe821
Yurii Karabas <1998uriyyo@gmail.com>
2021-02-13 18:42:05 +0200

    Unify aliases

--- demo_hc2021/brute_force_pizzas.h
+++ demo_hc2021/brute_force_pizzas.h
@@ -7,21 +7,8 @@
 #include <tuple>
 #include <string>
 #include <functional>
+#include "entities.h"
 
-struct pizza_hasher {
-    size_t operator()(const std::set<std::string>& s) const
-    {
-        //res = std::hash<std::string>(*s.begin());
-        //calculate hash here.
-        return 0;
-    }
-};
-
-
-using pizza_map = std::unordered_map<std::set<std::string>, std::vector<int>, pizza_hasher>;
-using pizza_map_iter = pizza_map::iterator;
-
-auto best_pizzas_for_2(pizza_map &map)
-{
+auto best_pizzas_for_2(Pizzas &map) {
     return std::make_tuple(map.begin(), map.begin());
 }
--- demo_hc2021/entities.h
+++ demo_hc2021/entities.h
@@ -13,10 +13,19 @@
 
 using namespace std;
 
+struct pizza_hasher {
+    size_t operator()(const std::set<std::string> &s) const {
+        //res = std::hash<std::string>(*s.begin());
+        //calculate hash here.
+        return 0;
+    }
+};
+
 using Pizza = set<string>;
 using PizzaIndex = int;
 using PizzaIndexes = vector<PizzaIndex>;
-using Pizzas = std::unordered_map<Pizza, PizzaIndex>;
+using Pizzas = std::unordered_map<std::set<std::string>, std::vector<int>, pizza_hasher>;
+using pizzas_iter = Pizzas::iterator;
 
 struct Delivery {
     size_t team;
