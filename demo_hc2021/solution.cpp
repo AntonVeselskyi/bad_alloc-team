bfc9b097c53cb4ea5032241ebf6f9eb90610c646
Yurii Matus <yurii.matus@synapse.com>
2021-02-13 19:25:57 +0200

    parse pizzas

--- demo_hc2021/solution.cpp
+++ demo_hc2021/solution.cpp
@@ -8,6 +8,41 @@ int main(int argc, char *argv[]) {
     FileParser parser(argv[1]);
 
     FileOutput output(string(argv[1]) + "_res");
+
+    Pizzas pizzas;
+    int pizzaCount, twoPersonTeamCount, threePersonTeamCount, fourPersonTeamCount;
+
+    parser.get_next_line(pizzaCount, twoPersonTeamCount, threePersonTeamCount, fourPersonTeamCount);
+
+    for (int i = 0; i < pizzaCount; ++i) {
+        int numberOfIngredients;
+        std::string ingredientsStr;
+        parser.get_next_line(numberOfIngredients);
+
+        std::set<string> ingredients;
+        for (int j = 0; j < numberOfIngredients; j++) {
+            std::string ingredient;
+            parser.get_next_line(ingredient);
+            ingredients.insert(ingredient);
+        }
+        if (pizzas.find(ingredients) != pizzas.end()) {
+            pizzas[ingredients].push_back(i);
+        } else {
+            std::vector<int> temp;
+            temp.push_back(i);
+            pizzas.insert({ingredients, temp});
+        }
+    }
+
+//    Plz don't remove me
+//    for (auto &pizza : pizzas) {
+//        for (const auto & it : pizza.first)
+//            cout << ' ' << it;
+//        for(const auto  & it : pizza.second){
+//            cout << ' ' << it;
+//        }
+//        cout << '\n';
+//    }
     //Usage example
     //output.write_pizza_result(PizzaIndexes vector)
 }
