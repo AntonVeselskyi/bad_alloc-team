098967be5b0065e82136d4af5b35b5ba72528e79
Yurii Karabas <1998uriyyo@gmail.com>
2021-02-13 17:09:55 +0200

    Add hc2021 template

---
+++ hc2021/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <utility>
+#include "getline.h"
+#include "output.h"
+#include "entities.h"
+
+
+int main(int argc, char *argv[]) {
+    FileParser parser(argv[1]);
+
+    FileOutput output(string(argv[1]) + "_res");
+}
