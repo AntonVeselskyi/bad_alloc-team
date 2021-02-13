277256bbc170a8cb96a26cdfca456b4d2bee4d41
Yurii Karabas <1998uriyyo@gmail.com>
2021-02-13 18:18:09 +0200

    Add entities

--- demo_hc2021/entities.h
+++ demo_hc2021/entities.h
@@ -5,4 +5,18 @@
 #ifndef HC2018_ENTITIES_H
 #define HC2018_ENTITIES_H
 
+#include <set>
+#include <list>
+#include <string>
+
+using namespace std;
+
+using Pizza = set<reference_wrapper<string>>;
+using PizzaIndex = size_t;
+
+struct Delivery {
+    size_t team;
+    list<PizzaIndex> pizzas;
+};
+
 #endif //HC2018_ENTITIES_H
