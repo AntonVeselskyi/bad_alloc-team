b14567f2cbafb6fb2b0d245c4c3ea120260f03b5
Yurii Karabas <1998uriyyo@gmail.com>
2021-02-25 20:28:47 +0200

    Add basic entities

--- hc2021/entities.h
+++ hc2021/entities.h
@@ -5,4 +5,32 @@
 #ifndef HC2018_ENTITIES_H
 #define HC2018_ENTITIES_H
 
+#include <vector>
+#include <list>
+#include <utility>
+
+using StreetID = size_t;
+using CarID = size_t;
+using IntersectionID = size_t;
+using TrafficLightSchedule = std::pair<StreetID, ssize_t>;
+
+struct Car {
+    CarID id;
+    size_t road_time = 0;
+    std::vector<StreetID> path;
+};
+
+struct Street {
+    StreetID id;
+    size_t duration;
+    std::vector<CarID> queue;
+};
+
+struct Intersection {
+    IntersectionID id;
+    std::list<StreetID> in;
+    std::list<StreetID> out;
+    std::vector<TrafficLightSchedule> schedule;
+};
+
 #endif //HC2018_ENTITIES_H
