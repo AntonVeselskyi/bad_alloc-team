fd3d20c165346b61405d4c5ce9ce4e830419991d
Yurii Karabas <1998uriyyo@gmail.com>
2021-03-11 20:23:15 +0200

    fix marker impl

--- reply2021/marker.h
+++ reply2021/marker.h
@@ -5,23 +5,25 @@
 void mark_as_covered(Antenna &antenna, Building &building, vector<vector<Building *>> &grid) {
     building.visited = true;
 
-    for (auto i = 0; i < antenna.range; i++) {
-        auto j = antenna.range - i;
+    auto marker = [&](auto x, auto y) {
+        if (!(0 <= y && y < grid.size()))
+            return;
+        if (!(0 <= x && x < grid[y].size()))
+            return;
 
-        auto marker = [&](auto x, auto y) {
-            if (!(0 <= y && y < grid.size()))
-                return;
-            if (!(0 <= x && x < grid[y].size()))
-                return;
+        auto b = grid[y][x];
+        if (b != nullptr)
+            b->visited = true;
+    };
 
-            auto b = grid[y][x];
-            if (b != nullptr)
-                b->visited = true;
-        };
+    for (auto r = antenna.range; r > 0; r--) {
+        for (auto i = 0; i < r; i++) {
+            auto j = r - i;
 
-        marker(antenna.x + j, antenna.y + i);
-        marker(antenna.x - j, antenna.y + i);
-        marker(antenna.x + j, antenna.y - i);
-        marker(antenna.x - j, antenna.y - i);
+            marker(antenna.x + i, antenna.y + j);
+            marker(antenna.x - i, antenna.y + j);
+            marker(antenna.x + i, antenna.y - j);
+            marker(antenna.x - i, antenna.y - j);
+        }
     }
 }
